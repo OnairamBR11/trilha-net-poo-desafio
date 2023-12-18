7bb520f14e3076c6b9f6e9a7e7036f7cc80f5bd0
Jose Mariano Barbosa Junior <jose.mariano@upaigarassu.intranet>
2023-12-18 14:56:42 -0300

    Resposta do Desafio

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -3,24 +3,21 @@ namespace DesafioPOO.Models
     public abstract class Smartphone
     {
         public string Numero { get; set; }
-        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
-
-        public Smartphone(string numero)
-        {
-            Numero = numero;
-            // TODO: Passar os parâmetros do construtor para as propriedades
-        }
+        private string Modelo { get; set; }
+        private string IMEI { get; set; }
+        private int Memoria { get; set; }
 
         public void Ligar()
         {
-            Console.WriteLine("Ligando...");
+            Console.WriteLine("Ligando o smartphone...");
         }
 
         public void ReceberLigacao()
         {
-            Console.WriteLine("Recebendo ligação...");
+            Console.WriteLine("Recebendo uma ligação...");
         }
 
-        public abstract void InstalarAplicativo(string nomeApp);
+        // Método abstrato que deverá ser implementado pelas classes derivadas
+        public abstract void InstalarAplicativo(string nome);
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,17 @@
 ﻿using DesafioPOO.Models;
+class Program
+{
+    static void Main()
+    {
+        Nokia nokia = new Nokia();
+        nokia.Ligar();
+        nokia.ReceberLigacao();
+        nokia.InstalarAplicativo("WhatsApp");
 
+        Iphone iphone = new Iphone();
+        iphone.Ligar();
+        iphone.ReceberLigacao();
+        iphone.InstalarAplicativo("Facebook");
+    }
+}
 // TODO: Realizar os testes com as classes Nokia e Iphone
